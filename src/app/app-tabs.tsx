e1cf5d18905e1ee847c8b5c094f415a1d6e409ef
shruthik shetty <shrutikshetty14@gmail.com>
2026-06-19 22:02:06 +0530

    fix styles

--- src/app/app-tabs.tsx
+++ src/app/app-tabs.tsx
@@ -3,21 +3,28 @@ import { NativeTabs } from "expo-router/unstable-native-tabs";
 // main tabs for the app
 const AppTabs = () => {
   return (
-    <NativeTabs>
+    <NativeTabs
+      iconColor={{
+        default: "#6b7280", // Gray-500
+        selected: "#15803d", // Green-700
+      }}
+      labelStyle={{
+        default: { color: "#6b7280" },
+        selected: { color: "#15803d", fontWeight: "bold" },
+      }}
+      backgroundColor="#ffffff"
+      disableIndicator={true}
+      shadowColor="#000000"
+      rippleColor={"gray"}
+    >
       <NativeTabs.Trigger name="home">
-        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon
-          src={require("@/assets/images/tabIcons/home.png")}
-          renderingMode="template"
-        />
+        <NativeTabs.Trigger.Label>Scan</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="camera.viewfinder" md="photo_camera" />
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="explore">
-        <NativeTabs.Trigger.Label>Explore</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon
-          src={require("@/assets/images/tabIcons/explore.png")}
-          renderingMode="template"
-        />
+        <NativeTabs.Trigger.Label>History</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="clock.fill" md="history" />
       </NativeTabs.Trigger>
     </NativeTabs>
   );
