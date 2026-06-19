106a8c3528f187b2046a3d994e2318332e6d15bf
shruthik shetty <shrutikshetty14@gmail.com>
2026-06-19 21:14:38 +0530

    add design

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -5,19 +5,40 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView className="flex-1 p-5 flex gap-5">
       {/* header */}
-      <Box className="flex-1 p-4">
+      <Box className="flex flex-row gap-5 p-4 items-center">
         {/* icon */}
-        <Ionicons name={"scan"} className="w-10 h-10" />
+        <Box className="p-2 rounded-full bg-green-800">
+          <Ionicons name={"scan"} size={25} color={"white"} />
+        </Box>
         {/* heading , sub title  */}
         <Box>
-          <Text size="xl" className="color-warning-300">
-            LabelScout
+          <Text size="2xl" className="color-typography-950" bold>
+            LabelScan
           </Text>
-          <Text size="md">AI-first deep label audit</Text>
+          <Text size="md" className="color-typography-600">
+            AI-first deep label audit
+          </Text>
         </Box>
       </Box>
+      {/* body title */}
+      <Box className="flex gap-5">
+        <Box>
+          <Text className="text-center color-typography-950" size="4xl" bold>
+            What's actually inside
+          </Text>
+          <Text className="text-center text-green-700" size="4xl" bold>
+            that packet?
+          </Text>
+        </Box>
+        <Text className="text-center color-typography-600">
+          Snap the label. Our AI nutritionist audits every single ingredient,
+          flags red ones, and scores it out of 10.
+        </Text>
+      </Box>
+
+      {/* scan component goes here  */}
     </SafeAreaView>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -175,9 +175,9 @@ module.exports = {
         },
       },
       fontFamily: {
-        heading: undefined,
-        body: undefined,
-        mono: undefined,
+        heading: ['System', 'sans-serif'],
+        body: ['System', 'sans-serif'],
+        mono: ['System', 'monospace'],
         jakarta: ['var(--font-plus-jakarta-sans)'],
         roboto: ['var(--font-roboto)'],
         code: ['var(--font-source-code-pro)'],
